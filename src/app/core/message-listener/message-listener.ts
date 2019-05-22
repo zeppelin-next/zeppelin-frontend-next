88c0cefc78f07437e156513b590e3df2f5fa5539
wb-ldx397571 <wb-ldx397571@alibaba-inc.com>
2019-05-22 10:53:51 +0800

    refactor: refactor MessageListener decorator

--- src/app/core/message-listener/message-listener.ts
+++ src/app/core/message-listener/message-listener.ts
@@ -18,14 +18,18 @@ export class MessageListenersManager implements OnDestroy {
   }
 }
 
-export function MessageListener(op: keyof MessageReceiveDataTypeMap) {
-  return function(target: MessageListenersManager, propertyKey: string, descriptor: PropertyDescriptor) {
-    const oldValue = descriptor.value;
+export function MessageListener<K extends keyof MessageReceiveDataTypeMap>(op: K) {
+  return function(
+    target: MessageListenersManager,
+    propertyKey: string,
+    descriptor: TypedPropertyDescriptor<(data?: Record<K, MessageReceiveDataTypeMap[K]>[K]) => void>
+  ) {
+    const oldValue = descriptor.value as (o: Record<K, MessageReceiveDataTypeMap[K]>[K]) => void;
 
     const fn = function() {
       this.__zeppelinMessageListeners$__.add(
         this.messageService.receive(op).subscribe(data => {
-          oldValue.apply(this, [data]);
+          oldValue.apply(this, [data as MessageReceiveDataTypeMap[K]]);
         })
       );
     };
